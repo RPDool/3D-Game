d67caf4c477bfc7ac0499568487d12d72075ea64
RPDool <rp.dool@student.alfa-college.nl>
2024-09-12 14:27:33 +0200

    gun
    gun

---
+++ 3D Game/Assets/Script/Gun.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GunData gunData;
+    [SerializeField] private Transform muzzle;
+
+    private float timeSinceLastShot;
+
+    private void Start()
+    {
+        PlayerShoot.shootInput += Shoot;
+    }
+
+    private bool CanShoot() => !gunData.reloading && timeSinceLastShot > 1f / (gunData.fireRate / 60f);
+
+    public void Shoot()
+    {
+        if (gunData.currentAmmo > 0)
+        {
+            if (CanShoot())
+            {
+                if (Physics.Raycast(muzzle.position, muzzle.forward, out RaycastHit hit, gunData.maxDistance))
+                {
+                    Debug.Log(hit.transform.name);
+                }
+
+                gunData.currentAmmo--;
+                timeSinceLastShot = 0f;
+                OnGunShot();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+
+        Debug.DrawRay(muzzle.position, muzzle.forward);
+    }
+
+    private void OnGunShot()
+    {}
+}
